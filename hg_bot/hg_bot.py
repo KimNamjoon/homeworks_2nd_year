e586e78f0070720a3a5fec58644a16cfc44db695
masatoru <akim_1@edu.hse.ru>
2019-10-09 11:33:28 +0300

    last one

--- hg_bot/hg_bot.py
+++ hg_bot/hg_bot.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     import os
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
