8be639ea231c82e78f082a98aecd59a9b6c337c8
masatoru <akim_1@edu.hse.ru>
2019-10-09 03:50:46 +0300

    a

--- hg_bot/hg_bot.py
+++ hg_bot/hg_bot.py
@@ -6,9 +6,6 @@ import re
 import flask
 
 bot = telebot.TeleBot("859243603:AAFGCSutKzT5Ksy4KOAEpPKMZM6zxGJ5PqA")
-require('https').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
-  res.end('')
-});
 
 app = flask.Flask(__name__)
 
@@ -16,7 +13,7 @@ def chain():
     with open('hg.txt', encoding='utf-8') as f:
         t = f.read()
     m = markovify.Text(t)
-    return m.make_short_sentence(max_chars=200)
+    return m.make_short_sentence(max_chars=100)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -32,4 +29,4 @@ if __name__ == '__main__':
     import os
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
